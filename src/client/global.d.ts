9926eead4472f59fde0a1b1af513e1f1d7ab930c
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-07-20 00:48:17 +0300

    Add isomorphic-style-loader declarations

---
+++ src/client/global.d.ts
@@ -0,0 +1,39 @@
+type Dispose = () => void;
+type InsertCssItem = () => Dispose;
+type GetCSSItem = () => string;
+type GetContent = () => string;
+
+interface Style {
+  [key: string]: InsertCssItem | GetCSSItem | GetContent | string;
+  _insertCss: InsertCssItem;
+  _getCss: GetCSSItem;
+  _getContent: GetContent;
+}
+
+declare module '*.module.scss' {
+  const style: Style;
+  export default style;
+}
+
+declare module '*.module.css' {
+  const style: Style;
+  export default style;
+}
+
+declare module 'isomorphic-style-loader/useStyles' {
+  export default function useStyles(...styles: Style[]): void;
+}
+
+declare module 'isomorphic-style-loader/StyleContext' {
+  import { Context } from 'react';
+
+  type RemoveGlobalCss = () => void;
+  type InsertCSS = (...styles: Style[]) => RemoveGlobalCss | void;
+  interface StyleContextValue {
+    insertCss: InsertCSS;
+  }
+
+  const StyleContext: Context<StyleContextValue>;
+
+  export { StyleContext as default, InsertCSS };
+}
